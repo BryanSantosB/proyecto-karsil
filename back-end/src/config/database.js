e0d40beb8e41ee6f5853a5153a47cd8528fe2c37
Bryan <bryanalexandersb20@gmail.com>
2026-02-04 11:06:08 -0500

    Removiendo archivos sensibles y agregando gitignore

--- back-end/src/config/database.js
+++ back-end/src/config/database.js
@@ -1,11 +1,24 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false // Vital para servicios externos y Render
-  }
-});
+const { Pool } = require('pg');
+
+// Esta línea es la clave: 
+// En Render usará DATABASE_URL. En tu PC, como no existe esa variable, 
+// usará los valores individuales de tu .env local.
+const pool = new Pool(
+  process.env.DATABASE_URL 
+    ? { 
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false } 
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
 
 pool.on("connect", () => {
   console.log("✅ Conectado a PostgreSQL");
